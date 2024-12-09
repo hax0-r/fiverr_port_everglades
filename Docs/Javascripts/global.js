ce207c1f8e9f3ba917b3625e2e47958e40ef02df
Muhammad <talha18513@gmail.com>
2024-12-09 14:48:16 +0500

    8 website done

--- Docs/Javascripts/global.js
+++ Docs/Javascripts/global.js
@@ -5,9 +5,15 @@ function raf(time) {
 }
 requestAnimationFrame(raf);
 
+
 const contactForm = document.getElementById("contactForm");
+const notify = document.getElementById("notify");
 const overlay = document.getElementById("overlay");
+const overlayMenu = document.getElementById("overlayMenu");
 const openContactForm = document.getElementsByClassName("openContactForm");
+const openMenuForm = document.getElementsByClassName("openMenuForm");
+const menuForm = document.getElementById("menuForm");
+const closeMenu = document.getElementById("closeMenu");
 
 Array.from(openContactForm).forEach(button => {
     button.addEventListener('click', () => {
@@ -29,4 +35,61 @@ overlay.addEventListener('click', () => {
         overlay.style.display = 'none';
         contactForm.style.display = 'none';
     }, 500);
+});
+
+Array.from(openMenuForm).forEach(button => {
+    button.addEventListener('click', () => {
+        overlayMenu.style.display = 'block';
+        menuForm.style.display = 'flex';
+
+        setTimeout(() => {
+            overlayMenu.style.opacity = '1';
+            menuForm.style.opacity = '1';
+        }, 50);
+    });
+});
+
+overlayMenu.addEventListener('click', () => {
+    overlayMenu.style.opacity = '0';
+    menuForm.style.opacity = '0';
+
+    setTimeout(() => {
+        overlayMenu.style.display = 'none';
+        menuForm.style.display = 'none';
+    }, 500);
+});
+closeMenu.addEventListener('click', () => {
+    overlayMenu.style.opacity = '0';
+    menuForm.style.opacity = '0';
+
+    setTimeout(() => {
+        overlayMenu.style.display = 'none';
+        menuForm.style.display = 'none';
+    }, 500);
+});
+
+let dismissed = false; 
+
+window.addEventListener("scroll", () => {
+    if (dismissed) return; 
+
+    if (window.scrollY > 400) {
+        notify.style.display = "block";
+        notify.style.opacity = "1";
+    } else {
+        notify.style.opacity = "0";
+        setTimeout(() => {
+            notify.style.display = "none";
+        }, 500);
+    }
+});
+
+document.addEventListener("click", (event) => {
+    if (!notify.contains(event.target)) {
+        notify.style.opacity = "0";
+        setTimeout(() => {
+            notify.style.display = "none";
+        }, 500);
+        dismissed = true;
+    }
 });
